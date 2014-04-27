a3999636ae38294cd8d851285d88b8ff1213c34b
James O'Hea <annex12@hotmail.com>
2014-04-28 00:17:13 +0100

    Built up the AI: Finds all playable cards, if none, pickup, if 1, play it, if > 1, split between magic and non-magic, try to use non-magic first following standard rule, otherwise magics

--- ComputerPlayer.py
+++ ComputerPlayer.py
@@ -13,7 +13,7 @@ class ComputerPlayer(Participant):
 		print "\n" + self.Name + " is holding:"
 		for Card in self.Hand:
 			print "    " + Card.Name + " of " + Card.Suit
-		print "    (" + str(self.HandScore()) + ")"
+		#print "    (" + str(self.HandScore()) + ")"
 		#self.DecideCard()
 
 	def HandValue(self):
@@ -29,26 +29,47 @@ class ComputerPlayer(Participant):
 	def DecideCard(self, PlayCardValue):
 		self.ListHand()
 		self.CardsToPlay = []
+		PlayableCards = []
 		CurrentHandValue = self.HandValue()
 		CurrentNumberCards = len(self.Hand)
 		#[y for y in x if y == 2]
-		print "Playable cards:"
+		
 		if (PlayCardValue == 7):
-			PlayableCards = [y.Value for y in self.Hand if y.Value <= PlayCardValue]
+			for y in self.Hand:
+				if (y.Value <= PlayCardValue) or (y.Value in (2,8,10)):
+					PlayableCards.append(y)
+					#PlayableCards = [y.Value for y in self.Hand if y.Value <= PlayCardValue]
 		else:
-			PlayableCards = [y.Value for y in self.Hand if y.Value >= PlayCardValue]
+			for y in self.Hand:
+				if (y.Value >= PlayCardValue) or (y.Value in (2,8,10)):
+					PlayableCards.append(y)
+					#PlayableCards = [y.Value for y in self.Hand if y.Value >= PlayCardValue or in (2,10,8)]
 
-		print PlayableCards
+		print "\n    Playable cards:"
 
 		if len(PlayableCards) == 0:
-			print "can't play!"
+			print "\n    Can't play! Picking up...."
 			return None
+################ New AI ####################
+		elif len(PlayableCards) == 1:
+			print "\n    Only 1 card to play"
+			self.CardsToPlay.append(PlayableCards[0])
+			return self.CardsToPlay
+		else:
+			#print "\n    More than 1 card to play"
+			non_magic = [i for i in PlayableCards if i.Value not in (2,8,10)]
+			magic = [j for j in PlayableCards if j.Value in (2,8,10)]
+			### careful - changing list type here!
+			PlayableCards = []
+			if(len(non_magic) == 0):
+				print "\n    All magic!"
+				PlayableCards = [y.Value for y in magic]
+			else:
+				print "\n    Non-magics available"
+				PlayableCards = [y.Value for y in non_magic]
+		print "    " + str(PlayableCards)
+############################################
 
-		print ""
-		CardValues = [i.Value for i in self.Hand]
-		OccValues = [self.Hand.count(Val) for Val in CardValues]
-
-		print "****************************"
 		BestScore = 0
 		Values = range(2,15)
 		my_dict = Counter(PlayableCards)
@@ -63,14 +84,15 @@ class ComputerPlayer(Participant):
 					PotentialHandValue = 1000
 				else:
 					PotentialHandScore = PotentialHandValue / (len(self.Hand) - multiply)
-				print "If " + self.Name + " played " + str(multiply) + " " + str(key) + "'s his hand score would become " + str(PotentialHandScore)
+				#print "If " + self.Name + " played " + str(multiply) + " " + str(key) + "'s his hand score would become " + str(PotentialHandScore)
 				if (PotentialHandScore > BestScore):
 					BestScore = PotentialHandScore
 					BestMultiply = multiply
 					BestValue = key
-		print "****************************"
+		
 
-		print "The best score is " + str(BestScore)
+		#print "The best score is " + str(BestScore)
+		print "****************************"
 		for i in range(0, BestMultiply):
 			for Card in self.Hand:
 				if Card.Value == BestValue:
@@ -78,7 +100,7 @@ class ComputerPlayer(Participant):
 					self.CardsToPlay.append(Card)
 					self.Hand.remove(Card)
 					break
-
+		print "****************************"
 		return self.CardsToPlay
 
 
